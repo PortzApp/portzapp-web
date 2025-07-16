8471680d3c993e8549e81ddfcbf2810629794415
Aryan Prince <aryanprince@portzapp.com>
2025-07-16 12:14:29 +0530

    feat(db): Add Service model, migration, setup 1-M relation

---
+++ app/Models/Service.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Service extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'status',
+        'user_id',
+    ];
+
+    /**
+     * Get the user that created the service.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
