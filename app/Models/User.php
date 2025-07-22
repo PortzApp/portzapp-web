ed658bda4f67fad767e4ced13beb6e837dc00832
Aryan Prince <aryanprince@portzapp.com>
2025-07-22 17:50:48 +0530

    feat: Add phone_number to fillable attributes in User model
    * Updated the User model to include 'phone_number' in the fillable attributes, allowing it to be mass assignable.

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,7 @@ class User extends Authenticatable
         'first_name',
         'last_name',
         'email',
+        'phone_number',
         'password',
         'role',
         'organization_id',
