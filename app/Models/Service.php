29e6d0198fd1949dea037a732fa2dd6d364b98a9
Aryan Prince <aryanprince@portzapp.com>
2025-07-23 17:36:39 +0530

    feat: Add Order model and establish relationship with Service model

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Service extends Model
 {
@@ -29,5 +30,13 @@ class Service extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the orders for the current service.
+     */
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
