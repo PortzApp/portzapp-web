138a59450c62fd96efe468c811ea45f6e6ad6204
Aryan Prince <aryanprince@portzapp.com>
2025-07-23 19:02:14 +0530

    refactor: Remove unnecessary prop from AppLogoIcon in AppLogo component
    * Simplified the AppLogo component by removing the 'className' prop from AppLogoIcon, enhancing code clarity and maintainability.

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,7 +3,7 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <div className="w-full flex justify-start">
-            <AppLogoIcon className="size-4" />
+            <AppLogoIcon />
         </div>
     );
 }
