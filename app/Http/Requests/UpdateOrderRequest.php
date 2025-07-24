5247f4a179c353060df5d8a05185d845708db92c
Aryan Prince <aryanprince@portzapp.com>
2025-07-24 14:27:36 +0530

    refactor: Update order update authorization and validation rules
    * Modified the authorization logic in UpdateOrderRequest to allow updates only for users with permission to update the specific order.
    * Added validation rules for the 'status' field to ensure it is required and limited to specific values, enhancing data integrity for order updates.

--- app/Http/Requests/UpdateOrderRequest.php
+++ app/Http/Requests/UpdateOrderRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateOrderRequest extends FormRequest
@@ -11,18 +12,20 @@ class UpdateOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        $order = $this->route('order');
+
+        return $this->user()->can('update', $order);
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'status' => 'required|in:pending,accepted,in_progress,completed,cancelled'
         ];
     }
 }
