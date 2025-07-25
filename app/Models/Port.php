c30742ebac0fed1cb85d7d4b67516906bffe08a8
Aryan Prince <aryanprince@portzapp.com>
2025-07-25 15:42:43 +0530

    feat: Enhance Port model and migration with fillable attributes
    * Updated Port model to include fillable attributes for name, code, status, country, city, latitude, longitude, and timezone, improving data handling.
    * Modified migration to create ports table with the necessary fields, ensuring proper structure for storing port information.

--- app/Models/Port.php
+++ app/Models/Port.php
@@ -2,11 +2,23 @@
 
 namespace App\Models;
 
+use Database\Factories\PortFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Port extends Model
 {
-    /** @use HasFactory<\Database\Factories\PortFactory> */
+    /** @use HasFactory<PortFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'status',
+        'country',
+        'city',
+        'latitude',
+        'longitude',
+        'timezone'
+    ];
 }
--- database/migrations/2025_07_25_091235_create_ports_table.php
+++ database/migrations/2025_07_25_091235_create_ports_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,14 @@ return new class extends Migration
     {
         Schema::create('ports', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('code', 10)->unique();
+            $table->string('status');
+            $table->string('country');
+            $table->string('city');
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 10, 8)->nullable();
+            $table->string('timezone');
             $table->timestamps();
         });
     }
