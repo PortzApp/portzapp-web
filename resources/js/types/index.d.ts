68d7fe81d3d31f13f421a94ed2b778db0f8c13b3
Aryan Prince <aryanprince@portzapp.com>
2025-07-28 17:13:56 +0530

    feat: Add Organization interface and update User model to include organization details
    * Introduced a new Organization interface defining properties such as id, name, business_type, and registration_code.
    * Updated the User interface to include an organization property, allowing for better association between users and their respective organizations.

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -35,6 +35,13 @@ export interface SharedData {
     [key: string]: unknown;
 }
 
+export interface Organization {
+    id: number;
+    name: string;
+    business_type: string;
+    registration_code: string;
+}
+
 export interface User {
     id: number;
     first_name: string;
@@ -44,6 +51,7 @@ export interface User {
     avatar?: string;
     email_verified_at: string | null;
     role: UserRoles;
+    organization: Organization | null;
     created_at: string;
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
