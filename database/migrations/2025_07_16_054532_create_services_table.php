09922a42415a47dbc6b3336ed99a124faebaea11
Aryan Prince <aryanprince@portzapp.com>
2025-07-29 23:09:54 +0530

    feat: Add port association to Service model and migration
    - Updated Service model to include a relationship with the Port model.
    - Modified the services migration to add a foreign key for port_id, ensuring proper associations and cascading updates/deletes.

--- database/migrations/2025_07_16_054532_create_services_table.php
+++ database/migrations/2025_07_16_054532_create_services_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,7 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->foreignId('organization_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('port_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', total: 10, places: 2);
