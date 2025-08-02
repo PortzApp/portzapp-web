1f583d2187a8e7ecf5a5f07e241890e213df55e9
Aryan Prince <aryanprince@portzapp.com>
2025-08-02 15:21:44 +0530

    feat: Add service_category_id to services and create service_categories migration
    - Added a new migration to create the service_categories table with a name column.
    - Updated the services table to include a foreign key reference to the service_categories table.

--- database/migrations/2025_08_02_062631_add_service_category_to_services_table.php
+++ database/migrations/2025_08_02_062631_add_service_category_to_services_table.php
@@ -10,10 +10,8 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('service_categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('services', function (Blueprint $table) {
+            $table->foreignId('service_category_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
         });
     }
 
@@ -22,6 +20,8 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('service_categories');
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropColumn('service_category_id');
+        });
     }
 };
