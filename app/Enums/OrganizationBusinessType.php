77828d64e73315c7a587086903cac03b360654c6
Aryan Prince <aryanprince@portzapp.com>
2025-08-05 11:59:41 +0530

    feat: Update OrganizationBusinessType enum to replace PLATFORM_ADMIN with PORTZAPP_TEAM

--- app/Enums/OrganizationBusinessType.php
+++ app/Enums/OrganizationBusinessType.php
@@ -6,14 +6,14 @@ enum OrganizationBusinessType: string
 {
     case SHIPPING_AGENCY = 'shipping_agency';
     case VESSEL_OWNER = 'vessel_owner';
-    case PLATFORM_ADMIN = 'platform_admin';
+    case PORTZAPP_TEAM = 'portzapp_team';
 
     public function label(): string
     {
         return match ($this) {
             self::SHIPPING_AGENCY => 'Shipping Agency',
             self::VESSEL_OWNER => 'Vessel Owner',
-            self::PLATFORM_ADMIN => 'Platform Admin',
+            self::PORTZAPP_TEAM => 'PortzApp Team',
         };
     }
 }
