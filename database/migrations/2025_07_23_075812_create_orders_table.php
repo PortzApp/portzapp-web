47f035d164c66e9a28a1b958b5de6e83d798fa4c
Aryan Prince <aryanprince@portzapp.com>
2025-08-08 12:49:27 +0530

    chore: Update orders migration to include new fields and remove order_vessel table

--- database/migrations/2025_07_23_075812_create_orders_table.php
+++ database/migrations/2025_07_23_075812_create_orders_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Enums\OrderStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->ulid('id')->primary();
-            $table->foreignUlid('requesting_organization_id')->constrained('organizations')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreignUlid('providing_organization_id')->constrained('organizations')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->decimal('price', 12, 2);
+            $table->string('order_number')->unique();
+            $table->foreignUlid('vessel_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignUlid('port_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignUlid('placed_by_user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignUlid('placed_by_organization_id')->constrained('organizations')->cascadeOnUpdate()->cascadeOnDelete();
             $table->text('notes')->nullable();
-            $table->enum('status', ['pending', 'accepted', 'in_progress', 'completed', 'cancelled'])->default('pending');
+            $table->string('status')->default(OrderStatus::PENDING->value)->index();
             $table->timestamps();
         });
     }
