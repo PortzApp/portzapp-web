c7017862be7311c8d906876ebd25304eda0948a5
Aryan Prince <aryanprince@portzapp.com>
2025-08-11 19:10:09 +0530

    refactor: Simplify route definitions by using resource routes for services, orders, and vessels

--- routes/web.php
+++ routes/web.php
@@ -20,29 +20,11 @@ Route::middleware(['auth', 'verified'])->group(function (): void {
 
     Route::put('/user/current-organization', SwitchOrganization::class)->name('user.current-organization.update');
 
-    Route::get('services', [ServiceController::class, 'index'])->name('services.index');
-    Route::get('services/create', [ServiceController::class, 'create'])->name('services.create');
-    Route::get('services/{service}', [ServiceController::class, 'show'])->name('services.show');
-    Route::post('services', [ServiceController::class, 'store'])->name('services.store');
-    Route::get('services/{service}/edit', [ServiceController::class, 'edit'])->name('services.edit');
-    Route::put('services/{service}', [ServiceController::class, 'update'])->name('services.update');
-    Route::delete('services/{service}', [ServiceController::class, 'destroy'])->name('services.destroy');
-
-    Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
-    Route::get('orders/create', [OrderController::class, 'create'])->name('orders.create');
-    Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
-    Route::post('orders', [OrderController::class, 'store'])->name('orders.store');
-    Route::get('orders/{order}/edit', [OrderController::class, 'edit'])->name('orders.edit');
-    Route::put('orders/{order}', [OrderController::class, 'update'])->name('orders.update');
-    Route::delete('orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
-
-    Route::get('vessels', [VesselController::class, 'index'])->name('vessels.index');
-    Route::get('vessels/create', [VesselController::class, 'create'])->name('vessels.create');
-    Route::get('vessels/{vessel}', [VesselController::class, 'show'])->name('vessels.show');
-    Route::post('vessels', [VesselController::class, 'store'])->name('vessels.store');
-    Route::get('vessels/{vessel}/edit', [VesselController::class, 'edit'])->name('vessels.edit');
-    Route::put('vessels/{vessel}', [VesselController::class, 'update'])->name('vessels.update');
-    Route::delete('vessels/{vessel}', [VesselController::class, 'destroy'])->name('vessels.destroy');
+    Route::resources([
+        'vessels' => VesselController::class,
+        'services' => ServiceController::class,
+        'orders' => OrderController::class,
+    ]);
 
     // Ports management routes (restricted to portzapp_team business type)
     Route::middleware('portzapp.team')->group(function (): void {
