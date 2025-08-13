dc83929d1de6c283765cb43f54e45feda85a533f
Aryan Prince <aryanprince@portzapp.com>
2025-08-13 08:55:48 +0530

    feat: Extend OrderService model with additional properties and methods for order management

--- app/Models/OrderService.php
+++ app/Models/OrderService.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Carbon;
  * @property int $id
  * @property int $order_id
  * @property int $service_id
+ * @property int|null $order_group_id
+ * @property int $quantity
+ * @property float $unit_price
+ * @property float $total_price
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @property-read Order $order
@@ -22,12 +26,23 @@ use Illuminate\Support\Carbon;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|OrderService whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|OrderService whereOrderId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|OrderService whereServiceId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|OrderService whereOrderGroupId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|OrderService whereQuantity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|OrderService whereUnitPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|OrderService whereTotalPrice($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|OrderService whereUpdatedAt($value)
  *
  * @mixin Eloquent
  */
 class OrderService extends Pivot
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'order_service';
+
     /**
      * The attributes that are mass assignable.
      *
@@ -36,5 +51,9 @@ class OrderService extends Pivot
     protected $fillable = [
         'order_id',
         'service_id',
+        'order_group_id',
+        'quantity',
+        'unit_price',
+        'total_price',
     ];
 }
