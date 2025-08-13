35034fc252bdf00bb23f277b49a3d261afc21303
Aryan Prince <aryanprince@portzapp.com>
2025-08-13 09:06:52 +0530

    feat: Add primary key to order_service table using ULID for unique identification

--- database/migrations/2025_08_03_050522_create_order_service_table.php
+++ database/migrations/2025_08_03_050522_create_order_service_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('order_service', function (Blueprint $table) {
+            $table->ulid('id')->primary();
             $table->foreignUlid('order_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignUlid('order_group_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignUlid('service_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
