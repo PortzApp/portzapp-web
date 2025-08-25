89d87fcf33b2c68bc0cd4ceb3564c53d81b77965
Aryan Prince <aryanprince@portzapp.com>
2025-08-25 16:51:35 +0530

    chore: Remove unused import

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,7 +6,6 @@ use App\Enums\ServiceStatus;
 use Eloquent;
 use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
